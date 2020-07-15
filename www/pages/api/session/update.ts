2e2fb7450c76cd2cec39cb16fc90683d1c9427cf
Pavel Ravits <vim55k@gmail.com>
2020-07-16 02:54:18 +0300

    delete token; dev env: opentok, fauna

---
+++ www/pages/api/session/update.ts
@@ -0,0 +1,26 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// import { graphQLClient, opentok, Session } from '~/pages/api/common'
+
+// let query = /* GraphQL */ `
+//     mutation UpdateSession($id: ID!) {
+//         updateSession(id: $id }) {
+//             id
+//             _id
+//         }
+//     }
+// `
+
+// export const updateSession = async (id: string) => graphQLClient.request(query, { id })
+
+// export default async (req: NextApiRequest, res: NextApiResponse) => {
+//     try {
+//         if (req.method !== 'POST') {
+//             throw Error('Only POST allowed')
+//         }
+//         res.status(200).end('nothing')
+//     } catch (error) {
+//         console.error(error)
+//         return res.status(error.status || 500).end(error.message)
+//     }
+// }
